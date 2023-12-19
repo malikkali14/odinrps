ef237ee6c0b2366784c84d708a9ca475a1ca0032
malikkali14 <126530790+malikkali14@users.noreply.github.com>
2023-12-19 09:57:37 -0500

    Created an html and js file to practice js further.

---
+++ js-file.js
@@ -0,0 +1,44 @@
+const container = document.querySelector('#container');
+const content = document.createElement('div');
+content.classList.add("content");
+content.textContent = 'This is the glorious text-content 4';
+container.setAttribute('style', 'background-color: green');
+
+container.appendChild(content);
+
+const redtxt = document.createElement('p');
+redtxt.textContent = "Hey I'm red!";
+redtxt.setAttribute('style', 'color:red');
+container.appendChild(redtxt);
+
+const bluetxt = document.createElement('h3');
+bluetxt.textContent = "Hey I'm a blue h3!";
+bluetxt.setAttribute('style', 'color:blue');
+container.appendChild(bluetxt);
+
+
+const newcntnt = document.createElement('div');
+newcntnt.classList.add('newcontent');
+const newtxt = document.createElement('h1');
+newtxt.textContent = "I'm in a div"
+newcntnt.appendChild(newtxt);
+container.appendChild(newcntnt);
+
+btn.addEventListener('click', function (e) {
+    console.log(e.target);
+  });
+
+btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+  });
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
